35df7a5120dade3a047b1bb03df283f3ef8f66ab
溫凊唯 <wenqingwei@MacBook-Air.home>
2024-05-11 01:28:12 +0800

    test

--- model.py
+++ model.py
@@ -29,27 +29,29 @@ class UNet(nn.Module):
         self.decoder3 = DoubleConv(128, 64)
         self.decoder4 = DoubleConv(64, out_channels)
         self.maxpool = nn.MaxPool2d(2)
-        self.upsample1 = nn.ConvTranspose2d(512, 128, kernel_size=2, stride=2)
+        self.upsample1 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
         self.upsample2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
         self.upsample3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.upsample4 = nn.ConvTranspose2d(64, out_channels, kernel_size=2, stride=2)
+        self.upsample4 = nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2)  # 将输出通道数调整为连接处的输出通道数
+
 
     def forward(self, x):
         x1 = self.encoder1(x)
         x2 = self.encoder2(self.maxpool(x1))
         x3 = self.encoder3(self.maxpool(x2))
         x4 = self.encoder4(self.maxpool(x3))
-
-        print("x4 size:", x4.size())
-        print("x3 size:", x3.size())
-
         
-        # 调整 x3 的尺寸，使其与 x4 的尺寸匹配
+        # 调整 x1、x2 和 x3 的尺寸，使其与 x4 的尺寸匹配
+        x1 = F.interpolate(x1, size=x4.size()[2:], mode='bilinear', align_corners=True)
+        x2 = F.interpolate(x2, size=x4.size()[2:], mode='bilinear', align_corners=True)
         x3 = F.interpolate(x3, size=x4.size()[2:], mode='bilinear', align_corners=True)
-        print("Interpolated x3 size:", x3.size())
+
+
+        # 对 x4 进行上采样操作
+        x4 = self.upsample1(x4)
         
         x = self.decoder1(torch.cat([self.upsample1(x4), x3], dim=1))
-        x = self.decoder2(torch.cat([self.upsample2(x), self.encoder2(x2)], dim=1))
-        x = self.decoder3(torch.cat([self.upsample3(x), self.encoder1(x1)], dim=1))
+        x = self.decoder2(torch.cat([self.upsample2(x), x2], dim=1))
+        x = self.decoder3(torch.cat([self.upsample3(x), x1], dim=1))
         x = self.decoder4(self.upsample4(x))
         return x
